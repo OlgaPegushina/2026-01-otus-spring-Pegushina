225045f324fcbc4947d9ea06a6b719659861b728
OlgaPegushina <pegushina.shine@yandex.ru>
2026-02-27 15:55:20 +0300

    ДЗ №3. Удалены лишние импорты после реализации автозапуска

--- hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/Application.java
@@ -2,8 +2,6 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
 public class Application {
