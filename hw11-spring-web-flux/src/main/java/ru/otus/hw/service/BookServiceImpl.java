0e715284765def38b4c003f33a276d5397f51bf8
OlgaPegushina <pegushina.shine@yandex.ru>
2026-06-17 20:24:10 +0300

    По рекомендации из ревью реализована проверка автора и жанров параллельным методом при создании книги

--- hw11-spring-web-flux/src/main/java/ru/otus/hw/service/BookServiceImpl.java
+++ hw11-spring-web-flux/src/main/java/ru/otus/hw/service/BookServiceImpl.java
@@ -11,9 +11,12 @@ import ru.otus.hw.dto.BookUpdateDto;
 import ru.otus.hw.exception.EntityNotFoundException;
 import ru.otus.hw.mapper.BookMapper;
 import ru.otus.hw.model.Book;
+import ru.otus.hw.repository.AuthorRepository;
 import ru.otus.hw.repository.BookRepository;
+import ru.otus.hw.repository.GenreRepository;
 
 import java.util.HashSet;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -22,6 +25,10 @@ public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
 
     private final BookMapper bookMapper;
+
+    private final AuthorRepository authorRepository;
+
+    private final GenreRepository genreRepository;
 
     @Override
     public Mono<BookDto> findById(long id) {
@@ -39,16 +46,12 @@ public class BookServiceImpl implements BookService {
     @Transactional
     public Mono<BookDto> create(BookCreateDto bookDto) {
         var genreIdsSet = new HashSet<>(bookDto.genreIds());
+        if (genreIdsSet.isEmpty()) {
+            return Mono.error(new IllegalArgumentException("Genres ids must not be empty"));
+        }
 
-        var bookToSave = new Book(0L, bookDto.title(), bookDto.authorId(), null, null);
-
-        return bookRepository.save(bookToSave)
-                .flatMap(savedBook -> {
-                    long bookId = savedBook.getId();
-                    return Flux.fromIterable(genreIdsSet)
-                            .flatMap(genreId -> bookRepository.saveGenreLink(bookId, genreId))
-                            .then(findById(bookId));
-                });
+        return validateDependencies(bookDto, genreIdsSet)
+                .then(Mono.defer(() -> saveBookAndLinks(bookDto, genreIdsSet)));
     }
 
     @Override
@@ -82,5 +85,37 @@ public class BookServiceImpl implements BookService {
     private Mono<Book> findBookOrError(long bookId) {
         return bookRepository.findById(bookId)
                 .switchIfEmpty(Mono.error(new EntityNotFoundException("Book with id %d not found".formatted(bookId))));
+    }
+
+    private Mono<Void> validateDependencies(BookCreateDto bookDto, Set<Long> genreIdsSet) {
+        Mono<Boolean> authorExists = authorRepository.existsById(bookDto.authorId());
+        Mono<Long> foundGenresCount = genreRepository.countByIdIn(genreIdsSet);
+
+        return Mono.zip(authorExists, foundGenresCount)
+                .flatMap(tuple -> {
+                    boolean isAuthorExists = tuple.getT1();
+                    long dbGenresCount = tuple.getT2();
+
+                    if (!isAuthorExists) {
+                        return Mono.error(new EntityNotFoundException(
+                                "Author with id %d not found".formatted(bookDto.authorId())));
+                    }
+                    if (dbGenresCount != genreIdsSet.size()) {
+                        return Mono.error(new EntityNotFoundException(
+                                "One or more genres with ids %s not found".formatted(genreIdsSet)));
+                    }
+                    return Mono.empty();
+                }).then(); //-- .then() преобразует Mono<Object> в Mono<Void>
+    }
+
+    private Mono<BookDto> saveBookAndLinks(BookCreateDto bookDto, Set<Long> genreIdsSet) {
+        var bookToSave = new Book(0L, bookDto.title(), bookDto.authorId(), null, null);
+
+        return bookRepository.save(bookToSave)
+                .flatMap(savedBook ->
+                        Flux.fromIterable(genreIdsSet)
+                                .flatMap(genreId -> bookRepository.saveGenreLink(savedBook.getId(), genreId))
+                                .then(findById(savedBook.getId()))
+                );
     }
 }
